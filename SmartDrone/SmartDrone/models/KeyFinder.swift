8826dd88c894a2572ed0f879eafdf0e3796e6482
Travis MacDonald <x2015khn@stfx.ca>
2019-08-09 16:01:24 -0300

    Started KeyFinder model overhead

---
+++ SmartDrone/SmartDrone/models/KeyFinder.swift
@@ -0,0 +1,9 @@
+//
+//  KeyFinder.swift
+//  SmartDrone
+//
+//  Created by IT Services Loaner on 2019-08-09.
+//  Copyright © 2019 Convergence Lab. All rights reserved.
+//
+
+import Foundation
---
+++ SmartDrone/SmartDrone/models/keyfinder/KeyFinder.swift
@@ -0,0 +1,9 @@
+//
+//  KeyFinder.swift
+//  SmartDrone
+//
+//  Created by Travis MacDonald on 2019-08-09.
+//  Copyright © 2019 Convergence Lab. All rights reserved.
+//
+
+import Foundation
