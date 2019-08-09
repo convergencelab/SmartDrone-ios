86cafd83ebdb1419e4c529717a92541717d24f51
Travis MacDonald <x2015khn@stfx.ca>
2019-08-09 15:12:50 -0300

    Added tests for melodic minor in active note list tests

--- SmartDrone/SmartDroneTests/ActiveNoteListTests.swift
+++ SmartDrone/SmartDroneTests/ActiveNoteListTests.swift
@@ -37,12 +37,13 @@ public class ActiveNoteListTests: XCTestCase {
         XCTAssertEqual(false, flag)
     }
     
-    /* TESTS FOR KEY STRENGTH */
+    /* TESTS FOR MAJOR SCALE */
     
     func testAddC() {
         let anList = ActiveNoteList()
         anList.addNote(ix: 0)
         XCTAssertEqual([1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0], anList.keyStrength)
+        XCTAssertEqual([true, false, false, false, false, false, false, false, false, false, false, false], anList.noteIsActive)
     }
     
     func testAddCAlreadyAdded() {
@@ -51,6 +52,7 @@ public class ActiveNoteListTests: XCTestCase {
         anList.addNote(ix: 0)
         // Should be same answer as only added once
         XCTAssertEqual([1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0], anList.keyStrength)
+        XCTAssertEqual([true, false, false, false, false, false, false, false, false, false, false, false], anList.noteIsActive)
     }
     
     func testAddRemoveC() {
@@ -58,18 +60,21 @@ public class ActiveNoteListTests: XCTestCase {
         anList.addNote(ix: 0)
         anList.removeNote(ix: 0)
         XCTAssertEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], anList.keyStrength)
+        XCTAssertEqual([false, false, false, false, false, false, false, false, false, false, false, false], anList.noteIsActive)
     }
     
     func testRemoveCFromEmptyList() {
         let anList = ActiveNoteList()
         anList.removeNote(ix: 0)
         XCTAssertEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], anList.keyStrength)
+        XCTAssertEqual([false, false, false, false, false, false, false, false, false, false, false, false], anList.noteIsActive)
     }
     
     func testAddB() {
         let anList = ActiveNoteList()
         anList.addNote(ix: 11)
         XCTAssertEqual([1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1], anList.keyStrength)
+        XCTAssertEqual([false, false, false, false, false, false, false, false, false, false, false, true], anList.noteIsActive)
     }
     
     func testAddCAndB() {
@@ -77,5 +82,60 @@ public class ActiveNoteListTests: XCTestCase {
         anList.addNote(ix: 0)
         anList.addNote(ix: 11)
         XCTAssertEqual([2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1], anList.keyStrength)
+        XCTAssertEqual([true, false, false, false, false, false, false, false, false, false, false, true], anList.noteIsActive)
+    }
+    
+    /* TESTS FOR MELODIC MINOR SCALE */
+    
+    func testAddCMel() {
+        let anList = ActiveNoteList()
+        anList.incrementSequence = MusicTheory.DORIAN_FLAT2_SEQUENCE
+        anList.addNote(ix: 0)
+        XCTAssertEqual([1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0], anList.keyStrength)
+        XCTAssertEqual([true, false, false, false, false, false, false, false, false, false, false, false], anList.noteIsActive)
+    }
+    
+    func testAddCAlreadyAddedMel() {
+        let anList = ActiveNoteList()
+        anList.incrementSequence = MusicTheory.DORIAN_FLAT2_SEQUENCE
+        anList.addNote(ix: 0)
+        anList.addNote(ix: 0)
+        // Should be same answer as only added once
+        XCTAssertEqual([1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0], anList.keyStrength)
+        XCTAssertEqual([true, false, false, false, false, false, false, false, false, false, false, false], anList.noteIsActive)
+    }
+    
+    func testAddRemoveCMel() {
+        let anList = ActiveNoteList()
+        anList.incrementSequence = MusicTheory.DORIAN_FLAT2_SEQUENCE
+        anList.addNote(ix: 0)
+        anList.removeNote(ix: 0)
+        XCTAssertEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], anList.keyStrength)
+        XCTAssertEqual([false, false, false, false, false, false, false, false, false, false, false, false], anList.noteIsActive)
+    }
+    
+    func testRemoveCFromEmptyListMel() {
+        let anList = ActiveNoteList()
+        anList.incrementSequence = MusicTheory.DORIAN_FLAT2_SEQUENCE
+        anList.removeNote(ix: 0)
+        XCTAssertEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], anList.keyStrength)
+        XCTAssertEqual([false, false, false, false, false, false, false, false, false, false, false, false], anList.noteIsActive)
+    }
+    
+    func testAddBMel() {
+        let anList = ActiveNoteList()
+        anList.incrementSequence = MusicTheory.DORIAN_FLAT2_SEQUENCE
+        anList.addNote(ix: 11)
+        XCTAssertEqual([1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1], anList.keyStrength)
+        XCTAssertEqual([false, false, false, false, false, false, false, false, false, false, false, true], anList.noteIsActive)
+    }
+    
+    func testAddCAndBMel() {
+        let anList = ActiveNoteList()
+        anList.incrementSequence = MusicTheory.DORIAN_FLAT2_SEQUENCE
+        anList.addNote(ix: 0)
+        anList.addNote(ix: 11)
+        XCTAssertEqual([2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1], anList.keyStrength)
+        XCTAssertEqual([true, false, false, false, false, false, false, false, false, false, false, true], anList.noteIsActive)
     }
 }
