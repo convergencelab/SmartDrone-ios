4f5260c8a32018cf68e62c5cdc281648ffbefff1
travismacdonald <x2015khn@stfx.ca>
2019-08-13 20:14:43 -0300

    KeyFinder protocol filled out

--- SmartDrone/SmartDrone/models/keyfinder/KeyFinder.swift
+++ SmartDrone/SmartDrone/models/keyfinder/KeyFinder.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+public protocol KeyFinder {
+
+    func start()
+
+    func clear()
+
+    func handleNote(noteIx: Int)
+
+    func getActiveKey() -> Key
+
+    func getKey(keyIx: Int) -> Key
+
+    func setKeyTimerLen(len: Int)
+
+    // Todo: Probably move this to note filter class
+    func setNoteLengthFilter(len: Int)
+
+    // Todo: Figure out way of dealing with this
+    // Enum class?
+    func setParentScale(parentScale: Int)
+
+    // Tdoo: getKey() will replace this
+    // ModeTemplate getModeTemplate(int templateIx);
+    // func getMode()
+
+    func addKeyChangeListener(listener: KeyChangeListener);
+
+    func removeKeyChangeListener(listener: KeyChangeListener);
+
+}
